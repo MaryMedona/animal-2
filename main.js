a5f44369dce1ad8e2fc59bf04cb989d90d4caf21
MaryMedona <128452174+MaryMedona@users.noreply.github.com>
2024-01-06 20:58:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function startclassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/daCCpRngE/',modelReady);
+
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
